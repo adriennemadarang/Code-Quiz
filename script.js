b171bf7863344b739732fbcbd008f33d5a876fb2
adriennemadarang <adriennemadarang@Adriennes-Air.attlocal.net>
2023-05-23 00:33:02 -0400

    added javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+// DOM elements
+var startButton = document.querySelector("#start");
+var timerEl = document.querySelector("#timer");
+var submitButton = document.querySelector("#submit");
+
+var time = questions.length * 5;
+var questions = [
+{
+    question:
+    choices: []
+    answer: 
+},
+{
+    question:
+    choices: []
+    answer:
+}, 
+{
+    question:
+    choices: []
+    answer:
+},
+{
+    question:
+    choices: []
+    answer:
+},
+
+
+]
+
+function startQuiz() {
+
+    var startScreenEl = document.getElementById("start-screen");
+    startScreenEl.setAttribute("class", "hidden");
+    timerId = setInterval (1000)
+}
+
+
+
+
